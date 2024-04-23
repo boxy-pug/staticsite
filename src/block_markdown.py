68cc5b0c34158ded40da2cda0b802b1d7db332d2
boxy-pug <151582186+boxy-pug@users.noreply.github.com>
2024-04-23 22:10:56 +0200

    added block to block type function with tests

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,8 +1,31 @@
+import re
+
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
 def markdown_to_blocks(markdown):
     split_blocks = markdown.split("\n\n")
     strip_blocks = []
     for block in split_blocks:
-        if block == "":
-            continue
         strip_blocks.append(block.strip())
     return strip_blocks
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+    heading_pattern = r"^#{1,6}\s+.*$"
+    if re.match(heading_pattern, lines[0]):
+        return block_type_heading
+    elif lines[0].startswith("```") and lines[-1].endswith("```"):
+        return block_type_code
+    elif all(line.startswith(">") for line in lines):
+        return block_type_quote
+    elif all(line.startswith(("* ", "- ")) for line in lines):
+        return block_type_unordered_list
+    elif all(line.startswith(f"{i + 1}. ") for i, line in enumerate(lines)):
+        return block_type_ordered_list
+    else:
+        return block_type_paragraph
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,9 +1,38 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_block_type
 
 class TestBlockMarkdown(unittest.TestCase):
     def test_markdown_to_blocks(self):
-        string = "This is **bolded** paragraph\n\nThis is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line\n\n* This is a list\n* with items"
+        string = "This is **bolded** paragraph\n\n\n\n"   "\n\n\nThis is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line\n\n* This is a list\n* with items\n\n\n\n\n\n"
         actual = markdown_to_blocks(string)
         expected = ["This is **bolded** paragraph", "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line", "* This is a list\n* with items" ]
- 
+    
+    def test_block_to_block_heading(self):
+        actual = block_to_block_type("### Hello there\nOkay")
+        expected = "heading"
+        self.assertEqual(actual, expected)
+
+    def test_block_to_block_code(self):
+        actual = block_to_block_type("```Hello there\nOkay```")
+        expected = "code"
+        self.assertEqual(actual, expected)
+
+    def test_block_to_block_quote(self):
+        actual = block_to_block_type("> Hello there\n>Okay```Shalom")
+        expected = "quote"
+        self.assertEqual(actual, expected)
+
+    def test_block_to_block_ul(self):
+        actual = block_to_block_type("* Hello there\n* Okay```Shaaa")
+        expected = "unordered_list"
+        self.assertEqual(actual, expected)
+
+    def test_block_to_block_ol(self):
+        actual = block_to_block_type("1. Hello there\n2. Okay```test\n3. Threeee")
+        expected = "ordered_list"
+        self.assertEqual(actual, expected)
+
+    def test_block_to_block_paragraph(self):
+        actual = block_to_block_type("Hello there\n2. Okay```test\n#. Threeee")
+        expected = "paragraph"
+        self.assertEqual(actual, expected)
